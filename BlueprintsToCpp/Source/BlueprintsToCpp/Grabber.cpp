0c5d57bf71d74c0a11a70c59625704414cc587ba
macro gu <macrogu@qq.com>
2020-05-20 11:29:44 +0800

    蓝图本地事件

---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Grabber.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Constructor"));
+
+
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber BeginPlay"));
+
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber TickComponent"));
+
+}
+
+FVector UGrabber::GetMaxGrabLocation() const
+{
+	
+	return GetComponentLocation() + GetComponentRotation().Vector() * MaxGrabDistance;
+
+}
+
+FVector UGrabber::GetHoldLocation() const
+{
+
+	return GetComponentLocation() + GetComponentRotation().Vector() * HoldDistance;
+
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsComponent() const
+{
+	return GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+}
+
+bool UGrabber::TraceForPhysicsBodies_Implementation(AActor*& HitActor, UPrimitiveComponent*& HitComponent)
+{
+
+
+	return true;
+}
---
+++ BlueprintsToCpp/Source/BlueprintsToCpp/Grabber.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent), Blueprintable)
+class BLUEPRINTSTOCPP_API UGrabber : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetMaxGrabLocation() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	FVector GetHoldLocation() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	UPhysicsHandleComponent* GetPhysicsComponent() const;
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void NotifyQuestActor(AActor* Actor);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool TraceForPhysicsBodies(AActor*& HitActor, UPrimitiveComponent*& HitComponent);
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+		
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float MaxGrabDistance = 100.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float HoldDistance = 100.f;
+
+};
