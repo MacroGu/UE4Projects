2466e35432e0461d1673f5058c65d032e41308a4
macro gu <macrogu@qq.com>
2021-04-17 00:12:11 +0800

    RPCCourse 8.exe后缀实现监听服务器

--- RPCCourse/Source/RPCCourse/RPCController.cpp
+++ RPCCourse/Source/RPCCourse/RPCController.cpp
@@ -2,14 +2,49 @@
 
 
 #include "RPCController.h"
+#include "ExCommon.h"
 
 
 void ARPCController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	FString SrceenCommand = FString("r.setres 1280x720w");
+	ConsoleCommand(SrceenCommand);
+
+
 	bShowMouseCursor = false;
 	FInputModeGameOnly InputMode;
 	SetInputMode(InputMode);
+
+	EchoNetMode();
+}
+
+void ARPCController::EchoNetMode()
+{
+	ENetMode NetMode = GetNetMode();
+	switch (NetMode)
+	{
+	case NM_Standalone:
+		DDH::Debug() << "NM_Standalone" << DDH::Endl();
+		break;
+	case NM_DedicatedServer:
+		DDH::Debug() << "NM_DedicatedServer" << DDH::Endl();
+		break;
+	case NM_ListenServer:
+		DDH::Debug() << "NM_ListenServer" << DDH::Endl();
+		break;
+	case NM_Client:
+		DDH::Debug() << "NM_Client" << DDH::Endl();
+		break;
+	case NM_MAX:
+		DDH::Debug() << "NM_MAX" << DDH::Endl();
+		break;
+	default:
+		DDH::Debug() << "default" << DDH::Endl();
+		break;
+	}
+
+
 
 }
--- RPCCourse/Source/RPCCourse/RPCController.h
+++ RPCCourse/Source/RPCCourse/RPCController.h
@@ -19,4 +19,6 @@ public:
 
 	virtual void BeginPlay() override;
 	
+	void EchoNetMode();
+
 };
