51c92bf13db31bb95823f97c1d7cdd44d3173f1b
macro gu <macrogu@qq.com>
2021-07-09 15:35:58 +0800

    demo for Minesweeper game in plugins

---
+++ Minesweeper/Source/Minesweeper/Minesweeper.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Minesweeper.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Minesweeper, "Minesweeper" );
---
+++ Minesweeper/Source/Minesweeper/MinesweeperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MinesweeperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINESWEEPER_API AMinesweeperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
